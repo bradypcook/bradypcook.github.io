4279b4bf93ab8b4115de789a5db6084bb1e562c6
LilCook <bcook468@gmail.com>
2025-10-30 22:27:49 -0400

    Typo Fix & Color Change

--- src/app/components/MainTitle.jsx
+++ src/app/components/MainTitle.jsx
@@ -1,15 +1,17 @@
 // Manages the main title section on the home page
+//#00F5D2 is the mercedes type
+//#00E701 is the sauber type
 
 export default function MainTitle() {
   return (
-    <div className="min-h-dvh flex flex-col justify-center items-center bg-gradient-to-b from-black to-indigo-900 text-white px-4 text-center">
+    <div className="min-h-dvh flex flex-col justify-center items-center bg-gradient-to-b from-black to-[#00E701] text-white px-4 text-center">
       <div className="max-w-2xl">
-        <h2 className="text-sm uppercase tracking-widest text-blue-400 mb-2">
+        <h2 className="text-sm uppercase tracking-widest text-lime-400 mb-2">
         Hello there! I'm 
       </h2>
         <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold">Brady Cook</h1>
         <p className="mt-4 text-gray-300 text-base sm:text-lg tracking-wide">
-            about computer networking, cybersecurity, programming, and much more!
+            and welcome to my personal website!
         </p>
       </div>
     </div>
--- src/app/components/SubTitle.jsx
+++ src/app/components/SubTitle.jsx
@@ -3,7 +3,7 @@
 
 export default function SubTitle({ title }) {
   return (
-    <div className="h-[30vh] flex flex-col justify-center items-center bg-gradient-to-b from-black to-indigo-900 text-white px-4 text-center">
+    <div className="h-[30vh] flex flex-col justify-center items-center bg-gradient-to-b from-black to-[#00E701] text-white px-4 text-center">
       <div className="max-w-2xl">
         <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold">{title}</h1>
       </div>
